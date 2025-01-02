325fcb80eee496f1658e707c9652668844d394b8
Shawn <sgiovanni1@yahoo.com>
2025-01-01 21:00:19 -0500

    added error formatter

--- backend/app.js
+++ backend/app.js
@@ -128,4 +128,18 @@ app.use((err, _req, res, _next) => {
   });
 });
 
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+    statusCode: err.status || 500
+  });
+});
+
+
 module.exports = app;
