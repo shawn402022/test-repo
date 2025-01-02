3a67f243f85b3dcefbe65e78f737d5fddba8887d
Shawn <sgiovanni1@yahoo.com>
2025-01-02 02:57:09 -0500

    MORE CIRCLES

--- frontend/src/store/testReducer.js
+++ frontend/src/store/testReducer.js
@@ -1,17 +1,18 @@
 const TEST = 'test/TEST';
 
 export const testAction = () => ({
+ 
   type: TEST,
-  payload: 'Test successful'
+    payload: 'Test successful'
 });
 
 const initialState = { message: null };
 
 export default function testReducer(state = initialState, action) {
-  switch (action.type) {
-    case TEST:
-      return { ...state, message: action.payload };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case TEST:
+            return { ...state, message: action.payload };
+        default:
+            return state;
+    }
 }
