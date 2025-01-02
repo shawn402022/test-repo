a683c42fcfa2e687c0af36c55b0070af14f6962b
Shawn <sgiovanni1@yahoo.com>
2025-01-01 19:59:16 -0500

    added console logs AGAIN

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+console.log('1. Before imports');
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -6,24 +8,28 @@ import ModalProvider from './components/Context/ModalContext';
 import configureStore from './store/store';
 import './index.css';
 
-const store = configureStore();
+console.log('2. After imports');
 
-// Create root and render application with test component
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = configureStore();
+console.log('3. Store created:', store);
+
+const rootElement = document.getElementById('root');
+console.log('4. Root element:', rootElement);
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
+    <div style={{background: 'red', padding: '20px'}}>
+      Mounting Check
+    </div>
     <Provider store={store}>
       <ModalProvider>
-        <div style={{
-          background: 'purple',
-          color: 'white',
-          padding: '50px',
-          fontSize: '24px',
-          textAlign: 'center'
-        }}>
-          React is Working!
+        <div style={{background: 'purple', padding: '20px'}}>
+          React + Redux Working
         </div>
         <Modal />
       </ModalProvider>
     </Provider>
   </React.StrictMode>
 );
+
+console.log('5. Render called');
